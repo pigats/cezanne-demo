3388bb765a109aea04896dda983665b3f782b258
pigats <andreapigato@gmail.com>
2015-10-08 15:39:36 +0100

    add appium example
    runs on saucelab, needs SAUCE_USERNAME and SAUCE_API_KEY

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -13,5 +13,29 @@ Cezanne.configure do |config|
   config.project_name = 'Demo'
 end
 
-Capybara.default_driver = :selenium
+Capybara.register_driver(:iphone6) do |app|
+  desired_caps_ios = { 
+    :name => "iPhone 6", 
+    :platformName => "iOS", 
+    :platformVersion => "8.4", 
+    :deviceName =>  "iPhone 6", 
+    :browserName => "Safari", 
+    :orientation => "portrait",  
+    :type => "mobile" 
+  }
+
+  appium_lib_options = {
+    server_url: 'http://#{ENV['SAUCE_USERNAME']}:#{ENV['SAUCE_API_KEY']}@ondemand.saucelabs.com:80/wd/hub'
+  }
+
+  all_options = {
+      appium_lib:  appium_lib_options,
+      caps:        desired_caps_ios
+  }
+
+  Appium::Capybara::Driver.new app, all_options
+end
+
+Capybara.default_driver = :iphone6
+
 Capybara.app_host = 'http://www.google.it' 
